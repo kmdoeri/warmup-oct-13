979b1db81ad968654f1fc12e9e7b059d745f311d
James Churchill <james@smashdev.com>
2020-10-13 12:20:10 -0700

    Update App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Testing git is helpful");
+
+        System.out.println("Why do java programmers wear glasses?");
+        System.out.println("Because they can’t C#");
     }
 }
